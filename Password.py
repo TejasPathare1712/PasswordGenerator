0eb77c87b9eb46d26cb2c3b5ffbaea545397673e
TejasPathare1712 <96852800+TejasPathare1712@users.noreply.github.com>
2023-02-26 21:03:39 +0530

    Create Password.py

---
+++ Password.py
@@ -0,0 +1,5 @@
+import random
+password = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%"
+length_password = int(input("Enter the length of password"))
+a = "".join(random.sample(password,length_password))
+print(f"Your Password is {a}")
